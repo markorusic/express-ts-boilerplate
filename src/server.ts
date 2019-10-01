2198eb28939e31db62fa6403d6bf2af6cc7982b5
Marko Rusic <marko.rusic@netcast.rs>
2019-10-02 01:08:53 +0200

    added deps ts config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => res.json({
-    message: 'zxc'
-}))
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req: Request, res: Response) =>
+  res.json({
+    message: 'hello'
+  })
+)
 
 app.listen(3000, () => {
-    console.log('running on port 3000')
+  console.log('running on port 3000')
 })
